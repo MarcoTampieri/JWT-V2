fa3e65eb76ba520b503af12c1095a0c7aa0a1ee3
Marco <mcg.tampieri@gmail.com>
2019-01-08 10:17:41 +0100

    POST works, token works, theoretically done

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ const User = require('./app/models/user');
 //*************
 
 let port = process.env.PORT || 8080;
-mongoose.connect(config.database, {useNewUrlParser: true}, (err) => {
+mongoose.connect(config.database, {
+    useNewUrlParser: true
+}, (err) => {
     if (err) throw err;
     console.log('connected to db')
 });
@@ -55,24 +57,90 @@ app.get('/setup', (req, res) => {
         admin: true,
     });
     bob.save()
-    .then(response => {
-        console.log(`${bob.name} saved succesfully`);
-        return res.json({success: true})
-    })
-    .catch(error => {
-        console.log(error, 'test')
-        return res.json({error: error});
-    })
+        .then(response => {
+            console.log(`${bob.name} saved succesfully`);
+            return res.json({
+                success: true
+            })
+        })
+        .catch(error => {
+            console.log(error, 'test')
+            return res.json({
+                error: error
+            });
+        })
 
-    
+
 });
 
 //Other routes
 
 let apiRoutes = express.Router();
 
+apiRoutes.post('/authenticate', (req, res) => {
+    User.findOne({
+        name: req.body.name
+    }, (err, user) => {
+        if (err) {
+            throw err
+        };
+
+        if (!user) {
+            res.json({
+                success: false,
+                message: "Authntication failed. User not found"
+            });
+        } else if (user) {
+            if (user.password != req.body.password) {
+                res.json({
+                    success: false,
+                    message: "Authentication failed. Inncorrect password"
+                })
+            } else {
+                const payload = {
+                    admin: user.admin,
+                };
+
+                let token = jwt.sign(payload, app.get('superSecret'), {
+                    expiresIn: 1440
+                });
+
+                res.json({
+                    success: true,
+                    message: "Have your token",
+                    token: token
+                })
+            }
+        }
+    });
+});
+
+//Protecting APIs
+
+apiRoutes.use((req, res, next) => {
+    let token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    if (token) {
+        jwt.verify(token, app.get('superSecret'), (err, decoded) => {
+            if (err) {
+                return res.json({
+                    success: false,
+                    message: 'Failed to authenticate token'
+                })
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        })
+    } else {
+        return res.status(403).send({success: false, message:'No token provided'});
+    }
+});
+
 apiRoutes.get("/", (req, res) => {
-    res.json({message: "Second test, for the api routes"})
+    res.json({
+        message: "Second test, for the api routes"
+    })
 });
 
 apiRoutes.get("/users", (req, res) => {
@@ -82,32 +150,6 @@ apiRoutes.get("/users", (req, res) => {
         }
         res.json(users)
     })
-});
-
-apiRoutes.post('/authenticate', (req, ers) => {
-    User.findOne({name: req.body.name}, (err, user) => {
-        if (err) {
-            throw err
-        };
-
-        if (!user) {
-            res.json({success: false, message: "Authntication failed. User not found"});
-        } else if (user) {
-            if (user.password != req.body.password) {
-                res.json({success: false, message: "Authentication failed. Inncorrect password"})
-            } else {
-                const payload = {
-                    admin: user.admin,
-                };
-
-                let token = jwt.sign(payload, app.get('superSecret'), {
-                    expiresInMinutes = 1440
-                });
-
-                res.json({success: true, message: "Have your token", token: token})
-            }
-        }
-    });
 });
 
 app.use('/api', apiRoutes);
